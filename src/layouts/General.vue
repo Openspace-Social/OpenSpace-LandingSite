ae3b951c7264792004fc7bf2f5595f95368a8313
massimoraimondi <massimo.raimondi@gmail.com>
2024-02-27 13:26:06 -0500

    added layouts folder

---
+++ src/layouts/General.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <index-navbar />
+    <main>
+        <div>
+        <br><br>
+        <p></p>
+        <about-us />
+        </div>
+    </main>
+    <footer-component />
+  </div>
+</template>
+<script>
+import IndexNavbar from "@/components/Navbars/IndexNavbar.vue";
+import FooterComponent from "@/components/Footers/Footer.vue";
+
+
+
+export default {
+  components: {
+    IndexNavbar,
+    FooterComponent,
+  },
+};
+</script>
