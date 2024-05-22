fe086aa18495f83cdbba44c98064ad20fbdc3ef9
massimoraimondi <massimo.raimondi@gmail.com>
2024-05-22 08:51:40 -0400

    add sign up on index page

--- src/components/Navbars/IndexNavbar.vue
+++ src/components/Navbars/IndexNavbar.vue
@@ -65,6 +65,17 @@
             </button>
           </li>
 </a>
+<a href="https://openspace.social/general/contact-us">
+          <li class="flex items-center">
+            <button
+              class="bg-blue-500 text-white active:bg-emerald-600 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
+              type="button"
+            >
+              <i class="fas fa-user lg:mr-2"></i> Sign-Up
+            </button>
+          </li>
+</a>
+
         </ul>
       </div>
     </div>
